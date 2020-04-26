892bfb48d9eae869c947eadfe54bf59892dedef4
Ibad Saladdin <ibad@Ibads-MacBook-Pro.local>
2020-04-26 13:57:28 +0700

    flutter challenge mi_card layouting

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100,
+                color: Colors.red,
+              ),
+              SizedBox(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  SizedBox(),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.yellow,
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                  SizedBox(),
+                ],
+              ),
+              SizedBox(),
+              Container(
+                width: 100,
+                color: Colors.blue,
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
